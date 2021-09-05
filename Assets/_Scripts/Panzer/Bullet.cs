095a46528a5e0bf0a37c1600151aafa469160a63
SuperNewPC\SuperPC <khaustov.maksym@gmail.com>
2021-09-05 20:03:18 +0300

    bullet now bounce from the obstacle

--- Assets/_Scripts/Panzer/Bullet.cs
+++ Assets/_Scripts/Panzer/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class Bullet : MonoBehaviour
 {
 
     [SerializeField] private float _speed;
+    [SerializeField] private float _bounceForce;
+    [SerializeField] private float _bounceRadius;
 
     private Vector3 _moveDirection;
 
@@ -28,5 +31,18 @@ public class Bullet : MonoBehaviour
             block.Break();
             Destroy(gameObject);
         }
+
+        if (otherCollider.TryGetComponent(out Obstacle obstacle))
+        {
+            Bounce();
+        }
+    }
+
+    private void Bounce()
+    {
+        _moveDirection = Vector3.forward + Vector3.up;
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.AddExplosionForce(_bounceForce, transform.position + new Vector3(0, -1, 1), _bounceRadius);
     }
 }
